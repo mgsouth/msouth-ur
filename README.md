5774b31a20a0860c4c995ecc016c6bfb381a892e
Michael South <msouth@msouth.org>
2021-07-16 03:19:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,191 @@
+# Installation & Setup, Linux or macOS:
+
+    $ git clone https://github.com/mgsouth/msouth-ur
+    $ cd msouth-ur
+    $ python3 -m venv venv
+    $ . venv/bin/activate
+    $ pip install click
+    $ pip install Flask
+
+Launch the service:
+
+    $ export FLASK_ENV=development
+    $ python ur.py
+
+Clear out the saved short urls:
+
+    $ rm redirs.*
+
+# Examples
+
+In a terminal session:
+
+    $ export FLASK_ENV=development
+    $ python ur.py
+
+In a separate terminal session:
+
+    $ curl -i -d '{"long_url": "http://whatever.com"}' 'http://127.0.0.1:5000/redirs/'
+
+=>
+
+    HTTP/1.0 200 OK
+    Content-Type: application/json
+    Content-Length: 155
+    Server: Werkzeug/2.0.1 Python/3.6.5
+    Date: Fri, 16 Jul 2021 09:42:22 GMT
+    
+    {
+      "reason": "",
+      "redir": {
+        "age": 0,
+        "long_url": "http://whatever.com",
+        "short_url": "http://127.0.0.1:5000/C"
+      },
+      "status": "OK"
+    }
+
+    $ curl -i -d '{"long_url": "https://en.wikipedia.org/wiki/URL_shortening"}' 'http://127.0.0.1:5000/redirs/'
+    $ curl -i -d '{"long_url": "http://127.0.0.1:5000/redirs/"}' 'http://127.0.0.1:5000/redirs/'
+    $ curl -i 'http://127.0.0.1:5000/redirs/'
+
+=>
+
+        HTTP/1.0 200 OK
+        Content-Type: application/json
+        Content-Length: 451
+        Server: Werkzeug/2.0.1 Python/3.6.5
+        Date: Fri, 16 Jul 2021 09:51:24 GMT
+        
+        {
+          "reason": "",
+          "redirs": [
+            {
+              "age": 390,
+              "long_url": "http://whatever.com",
+              "short_url": "http://127.0.0.1:5000/C"
+            },
+            {
+              "age": 369,
+              "long_url": "https://en.wikipedia.org/wiki/URL_shortening",
+              "short_url": "http://127.0.0.1:5000/D"
+            },
+            {
+              "age": 243,
+              "long_url": "http://127.0.0.1:5000/redirs/",
+              "short_url": "http://127.0.0.1:5000/E"
+            }
+          ],
+          "status": "OK"
+        }
+        
+-----
+
+In the following, we need the -L so that curl will actually follow the redirect.
+
+    $ curl -i -L http://127.0.0.1:5000/D
+=>
+
+        HTTP/1.0 302 FOUND
+        Content-Type: text/html; charset=utf-8
+        Content-Length: 294
+        Location: https://en.wikipedia.org/wiki/URL_shortening
+        Server: Werkzeug/2.0.1 Python/3.6.5
+        Date: Fri, 16 Jul 2021 09:52:34 GMT
+        
+        HTTP/2 200
+        date: Thu, 15 Jul 2021 20:15:58 GMT
+        vary: Accept-Encoding,Cookie,Authorization
+        server: ATS/8.0.8
+        x-content-type-options: nosniff
+        p3p: CP="See https://en.wikipedia.org/wiki/Special:CentralAutoLogin/P3P for more info."
+        content-language: en
+        last-modified: Mon, 12 Jul 2021 21:28:15 GMT
+        content-type: text/html; charset=UTF-8
+        age: 48996
+        ...
+        
+## Shortened POST:
+
+So meta... but it's a handy site which accepts POSTs
+
+    $ curl -i -d '{"long_url": "http://127.0.0.1:5000/redirs/"}' 'http://127.0.0.1:5000/redirs/'
+
+=>
+
+    "short_url": "http://127.0.0.1:5000/F"
+
+In the next command, note there's no trailing slash after the F.
+
+Without the --post302 curl with do a GET on the redirect rather than a POST.
+
+    $ curl -i -L --post302 -d '{"long_url": "https://news.ycombinator.com"}' 'http://127.0.0.1:5000/F'
+
+=>
+
+    HTTP/1.0 302 FOUND
+    Content-Type: text/html; charset=utf-8
+    Content-Length: 264
+    Location: http://127.0.0.1:5000/redirs/
+    Server: Werkzeug/2.0.1 Python/3.6.5
+    Date: Fri, 16 Jul 2021 09:58:07 GMT
+    
+    HTTP/1.0 200 OK
+    Content-Type: application/json
+    Content-Length: 164
+    Server: Werkzeug/2.0.1 Python/3.6.5
+    Date: Fri, 16 Jul 2021 09:58:07 GMT
+    
+    {
+      "reason": "",
+      "redir": {
+        "age": 0,
+        "long_url": "https://news.ycombinator.com",
+        "short_url": "http://127.0.0.1:5000/G"
+      },
+      "status": "OK"
+    }
+        
+# REST API
+
+    * Insert a long -> short redirection.
+
+        POST <host>/redirs/
+        { "long_url": <target> }
+
+    =>
+
+        { "status": "OK",
+          "reason": "",
+          "redir": {
+              "short_url":  <URL>,
+              "long_url":   <URL>,
+              "age":        <age of entry, in seconds, relative to current time>,
+          }
+        }
+
+    -or-
+
+        { "status": "ERROR",
+          "reason": <note describing error>,
+          "redir":  null,
+        }
+
+
+    * Get a list of all redirections.
+
+        GET <host>/redirs/
+
+    =>
+
+        { "status": "OK"
+          "reason": "",
+          "redirs": [
+              {
+                  "short_url":  <URL>,
+                  "long_url":   <URL>,
+                  "age":        <age of entry, in seconds, relative to current time>,
+              }
+              ...
+          ]
+        }
---
+++ ur.py
@@ -0,0 +1,194 @@
+import sqlite3
+import time
+from flask import Flask, g, json, jsonify, make_response, redirect, request, url_for
+
+# URL-shortening service.
+# Provides RESTful API for creating and querying shortened URLs. Redirects
+# short URLs to long targets.
+#
+# ***TOY APP, SECURITY NIGHTMARE***
+# No users, no authentication, no checks for malicious long URLs. Unexpected
+# SQL errors abort the program.
+
+# Base path for redirection REST API. I.E., requests will be to
+# HTTP://<host>/<APIROOT>/
+APIROOT = 'redirs'
+
+DATABASE='redirs.db'
+
+app = Flask(__name__)
+
+
+# DAO-ish
+
+# Internal and external views of REDIRS differ. Internal has a short-URL *path*,
+# external has a full URL. External synthesizes an 'age' column, internal has
+# a more-sensible timestamp. IRL you probably wouldn't want age, but that's the
+# literal ask in the problem and it makes for an interesting twist.
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    # We use sqlite's user_version pragma as the next short-url ID, and as a
+    # sentinel that the schema has already been configured.
+    with app.app_context():
+        with get_db() as db:
+            cur = db.execute('PRAGMA user_version')
+            v = cur.fetchone()
+            if v[0] == 0:
+                db.execute('CREATE TABLE redirs (short_path text primary key, long_url text, create_tstamp integer)')
+                db.execute('PRAGMA user_version=1')
+
+def short_path_to_url(sp):
+    sn_host, sn_port = None, None
+    sn = app.config.get('SERVER_NAME')
+    if sn is not None:
+        sn_host, _, sn_port = app.config.get('SERVER_NAME').partition(':')
+    return f'http://{sn_host or "127.0.0.1"}:{sn_port or 5000}/{sp}'
+
+def redir_to_ext(redir):
+    if redir is None: return None
+    return {
+        "short_url": short_path_to_url(redir['short_path']),
+        "long_url": redir['long_url'],
+        # It is possible for clock corrections to make time run backwards a bit,
+        # so pin age to >= 0.
+        "age": max(int(time.time()) - redir['create_tstamp'], 0),
+    }
+
+def find_all_redirs():
+    cur = get_db().execute('SELECT short_path, long_url, create_tstamp FROM redirs')
+    # IRL: Paging
+    rslt = cur.fetchall()
+    cur.close()
+    return [{"short_path": r[0], "long_url": r[1], "create_tstamp": r[2]} for r in rslt]
+
+def find_redir_short(short_path):
+    if short_path is None: return None
+    cur = get_db().execute('SELECT short_path, long_url, create_tstamp FROM redirs WHERE short_path = ?',
+        short_path)
+    rslt = cur.fetchall()
+    cur.close()
+    if len(rslt) > 0:
+        return {"short_path": rslt[0][0], "long_url": rslt[0][1], "create_tstamp": rslt[0][2]}
+    else:
+        return None
+
+def insert_redir(long_url):
+    # Note: avoid returning the word "redirs", since that is reserved by the
+    # REST API.
+    db = get_db()
+    redir = {
+        "short_path": None,
+        "long_url": long_url,
+        "create_tstamp": int(time.time())
+    }
+    # Keep creating short URLs until we get one that is valid and unused. In
+    # real life we'd want to create something deterministically unique across
+    # server instances and threads.
+    while True:
+        # Get the next short-url ID
+        cur = db.execute('PRAGMA user_version')
+        v = cur.fetchone()[0] + 1
+        cur.close()
+        db.execute(f'PRAGMA user_version={v}')
+
+        # Look, all I want to do is format an integer as base64. Is that too
+        # much to ask?
+        b64 = ''
+        alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_'
+        while True:
+            b64 = alpha[v % 64] + b64
+            v = v // 64
+            if v == 0: break
+        if b64.upper() == APIROOT.upper(): continue
+        redir['short_path'] = b64
+        try:
+            db.execute('INSERT INTO redirs (short_path, long_url, create_tstamp) VALUES (?, ?, ?)',
+                (b64, redir['long_url'], redir['create_tstamp']))
+        except sqlite3.IntegrityError:
+            # Duplicate key
+            continue
+        else:
+            db.commit()
+            break
+    return redir
+
+
+# REST API
+
+def sanitize_long(long_url):
+    rslt = (long_url if long_url is not None else '').lstrip().rstrip()
+    return rslt if rslt != '' else None
+
+def rslt_redir(status, reason, redir):
+    rslt = jsonify({
+        "status": status,
+        "reason": reason,
+        "redir": redir_to_ext(redir),
+    })
+    print(f'rslt_redir={rslt}')
+    return rslt
+
+def rslt_redir_list(status, reason, redirs):
+    rslt = {
+        "status": status,
+        "reason": reason,
+        "redirs": [],
+    }
+    if redirs is not None:
+        rslt["redirs"] = [redir_to_ext(rd) for rd in redirs]
+    return jsonify(rslt)
+
+@app.route(f'/{APIROOT}/', methods=['POST'])
+def post_redir():
+    req = request.get_json(force=True, silent=True)
+    lu = sanitize_long(req.get('long_url')) if req is not None else None
+    if lu is not None:
+        return rslt_redir('OK', '', insert_redir(lu))
+    else:
+        return {
+            "status": 'ERROR',
+            "reason": 'Expected json data {"long_url": VALUE}',
+            "redir": None,
+        }
+
+@app.route(f'/{APIROOT}/<path:short_path>', methods=['GET'])
+def get_redir_short(short_path):
+    redir = select_redir_short(short_path)
+    if redir is None:
+        return rslt_redir('ERROR', 'No such short path', None)
+    else:
+        return rslt_redir('OK', '', redir)
+    
+@app.route(f'/{APIROOT}/', methods=['GET'])
+def get_redirs():
+    return rslt_redir_list('OK', '', find_all_redirs())
+
+
+# Redirection service
+
+@app.route('/<string:short_path>', methods=['GET', 'POST', 'PUT'])
+def redir_short(short_path):
+    redir = find_redir_short(short_path)
+    if redir is None:
+        return make_response(render_template('error.html'), 404)
+    else:
+        return redirect(redir['long_url'])
+
+
+# Start
+
+if __name__ == '__main__':
+    init_db()
+    app.run()
